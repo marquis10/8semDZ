1d503d49fa27af81955eff742628ee5825599298
Alexandr Kuzmin <kuzmin_89@mail.ru>
2023-01-05 23:05:28 +0500

    56z_last

--- 56z/Program.cs
+++ 56z/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int stroka = 0, MinSum = matrix.GetLength(1) * 11;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            temp += matrix[i, j];
+        }
+        if (temp < MinSum)
+            {
+                MinSum = temp;
+                stroka = i;
+            }
+        Console.WriteLine($"Сумма {i} строки = {temp}");
+    }
+    Console.WriteLine($"{stroka} строка");
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальная матрица:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
